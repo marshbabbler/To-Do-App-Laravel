f1f638d723856f5e28cb50fd8b6ff2634303b593
Jigyashu Kumar Gupta <marshbabbler.official@gmail.com>
2025-04-28 19:52:01 +0530

    Day 2 28 April

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome', function () { // Path
+    return view('hello'); // Page
+});
+
+Route::get('/about', function () { // Path
+    return view('about'); // Page
+});
+
+Route::get('/contact-us', function () { // Path
+    return view('contact'); // Page
 });
