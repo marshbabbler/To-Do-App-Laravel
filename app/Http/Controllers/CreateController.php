3c99311a3a0edbd65ab72a02544d4d5d64a8786b
Jigyashu Kumar Gupta <marshbabbler.official@gmail.com>
2025-07-15 20:49:17 +0530

    update 15/07/2025

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\validator;
+
+use App\Models\Task;
 
 class CreateController extends Controller
 {
@@ -11,6 +14,17 @@ class CreateController extends Controller
     }
 
     public function store(Request $request) {
-        
+        $validateData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string|max:300',
+        ]);
+
+        Task::create([
+
+            'title' => $validateData['title'],
+            'description' => $validateData['description']
+        ]);
+
+        return redirect()->back()->with('success', 'Your Task has been created...');
     }
 }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,16 +5,21 @@
 @section('content')
     <div class="container">
         <form action='{{ route('taskCreate') }}' method="POST">
+            @csrf
+
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title">
-                {{-- <div id="emailHelp" class="form-text"></div> --}}
+                <input type="text" class="form-control @error('title') border-danger @enderror" id="title" name="title" value="{{ old('title')}}">
+                @error('title')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                {{-- <input type="text" class="form-control" id="description" name="description"> --}}
-                <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
-                {{-- <div id="emailHelp" class="form-text"></div> --}}
+                <textarea class="form-control" name="description" id="description" cols="30" rows="10">{{ old('description')}}</textarea>
+                @error('description')
+                    <p>{{ $message }}</p>
+                @enderror
             </div>
             
             <button type="submit" class="btn btn-primary">Submit</button>
